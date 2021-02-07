6fdd08a22afb5d35e8e1d234e7ec3bebb3037e92
Jacob Manning <manningjc@g.cofc.edu>
2021-02-07 00:14:27 -0500

    Buttons Operational

--- app/src/main/java/com/example/button_counter_manning/MainActivity.java
+++ app/src/main/java/com/example/button_counter_manning/MainActivity.java
@@ -1,38 +1,55 @@
 package com.example.button_counter_manning;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.widget.Button;
 import android.view.View;
 import android.widget.TextView;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button addB;
     private Button minusB;
     private Button quitB;
-    private boolean buttonPressed = false;
+    private Button resetB;
     private TextView text;
-
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = this.findViewById(R.id.textView);
         addB = this.findViewById(R.id.button);
-        addB.setOnClickListener(new View.OnClickListener() {
+        minusB = this.findViewById(R.id.button2);
+        resetB = this.findViewById(R.id.button3);
+        quitB = this.findViewById(R.id.button4);
 
-            @Override
-            public void onClick(View v) {
-                if(!buttonPressed) {
-                    buttonPressed = true;
-                    String count = getString(R.string.counter);
-                    count = count + 1;
+        addB.setOnClickListener(v -> {
+            counter++;
+            text.setText(Integer.toString(counter));
+            });
 
-
-                }
-
-            }
+        minusB.setOnClickListener(v -> {
+            counter--;
+            text.setText(Integer.toString(counter));
         });
+
+        resetB.setOnClickListener(v -> {
+            counter = 0;
+            text.setText(Integer.toString(counter));
+        });
+
+        quitB.setOnClickListener(v -> {
+            finish();
+            System.exit(0);
+        });
+    }
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }

862849269dc48e2f11fe3b8d7e3618076d7b5ef6
Jacob Manning <manningjc@g.cofc.edu>
2021-02-09 18:53:40 -0500

    Clean Up Landscape View and Add Instance States

--- app/src/main/java/com/example/button_counter_manning/MainActivity.java
+++ app/src/main/java/com/example/button_counter_manning/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.button_counter_manning;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,11 +12,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG="MainActivity";
     private Button addB;
     private Button minusB;
     private Button quitB;
     private Button resetB;
-    private TextView text;
+    private TextView display;
     int counter = 0;
 
     @Override
@@ -23,31 +25,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = this.findViewById(R.id.textView);
+        display = this.findViewById(R.id.textView);
         addB = this.findViewById(R.id.button);
         minusB = this.findViewById(R.id.button2);
         resetB = this.findViewById(R.id.button3);
         quitB = this.findViewById(R.id.button4);
 
+
         addB.setOnClickListener(v -> {
             counter++;
-            text.setText(Integer.toString(counter));
+            display.setText(Integer.toString(counter));
             });
 
         minusB.setOnClickListener(v -> {
             if (counter == 0) {
-                text.setText(Integer.toString(counter));
+                display.setText(Integer.toString(counter));
                 displayToast("Cannot have a Negative of Something");
             }
             else {
                 counter--;
-                text.setText(Integer.toString(counter));
+                display.setText(Integer.toString(counter));
             }
         });
 
         resetB.setOnClickListener(v -> {
             counter = 0;
-            text.setText(Integer.toString(counter));
+            display.setText(Integer.toString(counter));
         });
 
         quitB.setOnClickListener(v -> {
@@ -58,4 +61,55 @@ public class MainActivity extends AppCompatActivity {
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        String counterValue = display.getText().toString();
+        outState.putString("display", counterValue);
+     }
+
+     @Override
+     protected void onRestoreInstanceState(@NonNull Bundle saveInstanceState) {
+        super.onRestoreInstanceState(saveInstanceState);
+        String counterValue = saveInstanceState.getString("display");
+        display.setText(counterValue);
+     }
+
+    /*
+    * @Override
+    * protected void onStart() {
+    *   super.onStart();
+    *   Log.i(TAG, "I entered onStart()");
+    * }
+    *
+    * @Override
+    * protected void onResume() {
+    *   super.onResume();
+    *   Log.i(TAG, "I entered onResume()");
+    * }
+    *
+    * @Override
+    * protected void onPause() {
+    *   super.onPause();
+    *   Log.i(TAG, "I entered onPause()");
+    * }
+    *
+    * @Override
+    * protected void onStop() {
+    *   super.onStop();
+    *   Log.i(TAG, "I entered onStop()");
+    * }
+    *
+    * @Override
+    * protected void onRestart() {
+    *   super.onRestart();
+    *   Log.i(TAG, "I entered onRestart()");
+    * }
+    *
+    * @Override
+    * protected void onDestroy() {
+    *   super.onDestroy();
+    *   Log.i(TAG, "I entered onDestroy()");
+    */
 }

621ca64b1a02b85825b0a9a8886ee8ef2bdea0c4
Jacob Manning <manningjc@g.cofc.edu>
2021-02-09 19:10:18 -0500

    Add Lifecycle

--- app/src/main/java/com/example/button_counter_manning/MainActivity.java
+++ app/src/main/java/com/example/button_counter_manning/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.widget.TextView;
@@ -18,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
     private Button quitB;
     private Button resetB;
     private TextView display;
-    int counter = 0;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        display = this.findViewById(R.id.textView);
+        display = this.findViewById(R.id.displayID);
         addB = this.findViewById(R.id.button);
         minusB = this.findViewById(R.id.button2);
         resetB = this.findViewById(R.id.button3);
@@ -76,40 +77,39 @@ public class MainActivity extends AppCompatActivity {
         display.setText(counterValue);
      }
 
-    /*
-    * @Override
-    * protected void onStart() {
-    *   super.onStart();
-    *   Log.i(TAG, "I entered onStart()");
-    * }
-    *
-    * @Override
-    * protected void onResume() {
-    *   super.onResume();
-    *   Log.i(TAG, "I entered onResume()");
-    * }
-    *
-    * @Override
-    * protected void onPause() {
-    *   super.onPause();
-    *   Log.i(TAG, "I entered onPause()");
-    * }
-    *
-    * @Override
-    * protected void onStop() {
-    *   super.onStop();
-    *   Log.i(TAG, "I entered onStop()");
-    * }
-    *
-    * @Override
-    * protected void onRestart() {
-    *   super.onRestart();
-    *   Log.i(TAG, "I entered onRestart()");
-    * }
-    *
-    * @Override
-    * protected void onDestroy() {
-    *   super.onDestroy();
-    *   Log.i(TAG, "I entered onDestroy()");
-    */
+     @Override
+     protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "I entered onStart()");
+     }
+
+     @Override
+     protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "I entered onResume()");
+     }
+
+     @Override
+     protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "I entered onPause()");
+     }
+
+     @Override
+     protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "I entered onStop()");
+     }
+
+     @Override
+     protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "I entered onRestart()");
+     }
+
+     @Override
+     protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "I entered onDestroy()");
+     }
 }
